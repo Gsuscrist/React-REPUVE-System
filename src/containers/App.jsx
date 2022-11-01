816eefcfb7df2942b199a939724fd3010731c2be
Gsuscrist <213359@ids.upchiapas.edu.mx>
2022-11-01 00:51:24 -0600

    [Start]

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+function App(){
+    return(
+        <>
+            <Header></Header>
+            <Footer></Footer>
+        </>
+
+
+    );
+}
+export default App;
