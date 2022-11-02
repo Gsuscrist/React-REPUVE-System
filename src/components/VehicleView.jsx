1ca093adf2e9af02067e2bc5c288d423f4a7fc17
Gsuscrist <213359@ids.upchiapas.edu.mx>
2022-11-02 16:01:43 -0600

    [ADD] layout vehicle view

---
+++ src/components/VehicleView.jsx
@@ -0,0 +1,53 @@
+import car from '../assets/imgs/car.jpg'
+import '../assets/styles/VehicleView.css'
+function VehicleView(){
+
+    return (
+        <>
+            <div className={"vehicleview"}>
+                <input className={"vehicleData"} value={"Car model"} disabled={true}/>
+        <img className={"vehiclePic"} src={car} alt={"car picture"}/>
+                <div className={"information"}>
+                    <div className={"infoBox"}>
+                        <label> brand:</label>
+                        <input readOnly={true} id={"brandInfo"} value={"null"} className={"infoV"} disabled={true}></input>
+                    </div>
+                    <br/>
+                    <div className={"infoBox"}>
+                        <label> model:</label>
+                        <input  readOnly={true} id={"modelInfo"} value={"null"} className={"infoV"} disabled={true}></input>
+                    </div>
+                    <br/>
+                    <div className={"infoBox"}>
+                        <label> year:</label>
+                        <input readOnly={true}  id={"yearInfo"} value={"null"} className={"infoV"} disabled={true}></input>
+                    </div>
+                    <br/>
+                    <div className={"infoBox"}>
+                        <label> color:</label>
+                        <input readOnly={true}  id={"colorInfo"} value={"null"} className={"infoV"} disabled={true}></input>
+                    </div>
+                    <br/>
+                    <div className={"infoBox"}>
+                        <label> fuel:</label>
+                        <input readOnly={true}  id={"fuelInfo"} value={"null"} className={"infoV"} disabled={true}></input>
+                    </div>
+                    <br/>
+                    <div className={"infoBox"}>
+                        <label> plate:</label>
+                        <input readOnly={true}  id={"plateInfo"} value={"null"} className={"infoV"} disabled={true}></input>
+                    </div>
+                    <br/>
+                    <div className={"infoBox"}>
+                        <label> status:</label>
+                        <input readOnly={true}  id={"statusInfo"} value={"null"} className={"infoV"} disabled={true}></input>
+                    </div>
+                </div>
+            </div>
+            <button className="button-28" role="button"> Volver </button>
+
+
+        </>
+    );
+}
+export default VehicleView;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import VehicleView from "../components/VehicleView";
 function App(){
     return(
         <>
             <Header></Header>
+            <VehicleView></VehicleView>
             <Footer></Footer>
         </>
 
