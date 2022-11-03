6f71263ddc07c342ceda499e8ca0d84e1a7992cd
EczarEsco <eczar22029@gmail.com>
2022-11-03 12:30:51 -0600

    create car register

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Formulario from "../components/Formulario.jsx";
 function App(){
     return(
         <>
-            <Header></Header>
-            <Footer></Footer>
+            <Header/>
+            <Formulario/>
+            <Footer/>
         </>
 
 
