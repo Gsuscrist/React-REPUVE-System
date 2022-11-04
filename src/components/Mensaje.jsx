692a7c69704fc0b21c7135ecacee8bb0735a56fd
Eduardo Huerta <90126777+EduardoVH@users.noreply.github.com>
2022-11-03 20:57:58 -0600

    Add files via upload

---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,10 @@
+import '../assets/styles/Mensaje.css';
+function Mensaje() {
+    return (
+        <div className="container-mensaje">
+            <span className="Mensaje">POR FAVOR INGRESE LOS DATOS COMO SE LE INDICA </span>
+        </div>
+    );
+}
+
+export default Mensaje;
